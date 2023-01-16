c224c9f4cba8c3304d74088117b62dd0e00664c5
alexsenokosov <alexsenokosov666@gmail.com>
2023-01-16 21:05:54 +0300

    DZ3

--- WeatherApp2/weather/views.py
+++ WeatherApp2/weather/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
+import requests
 
 # Create your views here.
 
 def index(request):
+    appid = '0fedf62a4b48093d0fe6e0b95fe05ae2'
+    url = 'https://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=' + appid
+    city = "Minsk"
+    res = requests.get(url.format(city))
+    print(res.text)
+
+
+
+
     return render(request,'weather/index.html')
